0482398b4676a72fff8fd5a6580dae0a3daf814c
navsingh3211 <navsingh3211@gmail.com>
2023-12-17 02:38:16 +0530

    pprw

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsMedium} from 'react-icons/bs'
+
 
 const HeaderSocials = () => {
   return (
